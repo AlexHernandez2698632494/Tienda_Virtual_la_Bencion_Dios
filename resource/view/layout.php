3f95fab3152899286be780c09bb654523d59169f
Cartagena2000 <144555706+Cartagena2000@users.noreply.github.com>
2024-08-16 23:17:51 -0600

    Footer

--- resource/view/layout.php
+++ resource/view/layout.php
@@ -19,7 +19,7 @@
 
     <div class="nav-bar">
         <a href="#">Inicio</a>
-        <a href="#">Corporativo</a>
+        <a href="Corporativo.php">Corporativo</a>
         <a href="#">Contacto</a>
         <div class="dropdown">
             <a href="#" class="dropbtn">Productos ▼</a>
@@ -36,11 +36,34 @@
         <?php if (isset($pageContent)) echo $pageContent; ?>
     </div>
 
-    <div class="footer">
-        Footer
-    </div>
+    <footer class="footer">
+        <div class="footer-content">
+            <div class="footer-section categorias">
+                <h2>Categorías</h2>
+                <ul>
+                    <li><a href="#">Dunlop</a></li>
+                    <li><a href="#">Falken</a></li>
+                    <li><a href="#">Sumitomo</a></li>
+                </ul>
+            </div>
+            <div class="footer-section contacto">
+                <h2>Contacto</h2>
+                <p>Carretera Troncal del Norte, La Palma</p>
+                <p>Tel: <a href="tel:+50379888963">+(503)7988-8963</a></p>
+                <p>Email: <a href="mailto:jaimesalguero233@gmail.com">jaimesalguero233@gmail.com</a></p>
+                <div class="socials">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-whatsapp"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
 
     <!-- Scripts de Bootstrap y JavaScript necesarios -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
